383a45d135c33a2a176470e4839632a7b8a4d775
Justin Wong <justinwong12337@gmail.com>
2014-10-02 22:36:20 -0600

    Optimized loading speed. Attempt at fixing slider loading issue

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
+$(document).ready(function() {
+    $("#lightSlider").lightSlider(); 
 
+    var site = $('#site');
+            
+    $(document).ready(function() {
+        $('#lightSlider').lightSlider({
+            minSlide:1,
+            maxSlide:1,
+            keyPress:false,
+            speed:2000,
+            auto:true,
+            controls:false,
+            onSliderLoad: function() {
+                $('#content-slider').removeClass('cS-hidden');
+            } 
+        });
+    });
+
+    /*$(window).load(function() {
+        if ($(window).width() <= 691) {
+               $ disable hover;
+        }
+    });*/
+});
