b699c266cbcdbad9fbfc1a9d32fed95c674ec6f7
Justin Wong <justinwong12337@gmail.com>
2014-10-28 20:16:04 -0600

    Adding under construction image and changing some wording

--- js/services/gmapsService.js
+++ js/services/gmapsService.js
@@ -4,7 +4,7 @@ define(["servicesFactory"], function(servicesFactory) {
 		var gmapsService = {
 
 			init_map: function(){
-				var myOptions = {zoom:14,center:new google.maps.LatLng(43.674657,-79.28708540000002),mapTypeId: google.maps.MapTypeId.ROADMAP};map = new google.maps.Map(document.getElementById("gmap_canvas"), myOptions);marker = new google.maps.Marker({map: map,position: new google.maps.LatLng(43.674657, -79.28708540000002)});infowindow = new google.maps.InfoWindow({content:"<b>ZONE SIX DESIGN BUILD LTD</b><br/>17 Cedar Aveunue<br/> Toronto" });google.maps.event.addListener(marker, "click", function(){infowindow.open(map,marker);});infowindow.open(map,marker);
+				var myOptions = {zoom:14,center:new google.maps.LatLng(43.684135,-79.30976609999999),mapTypeId: google.maps.MapTypeId.ROADMAP};map = new google.maps.Map(document.getElementById("gmap_canvas"), myOptions);marker = new google.maps.Marker({map: map,position: new google.maps.LatLng(43.684135,-79.30976609999999)});infowindow = new google.maps.InfoWindow({content:"<b>ZONE SIX DESIGN BUILD LTD</b><br/>17 Cedarvale Aveunue<br/> Toronto" });google.maps.event.addListener(marker, "click", function(){infowindow.open(map,marker);});infowindow.open(map,marker);
 				google.maps.event.addDomListener(window, 'load', gmapsService.init_map);
 			}
 		}
@@ -12,3 +12,4 @@ define(["servicesFactory"], function(servicesFactory) {
 		return gmapsService;
 	});
 });
+
